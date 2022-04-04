b92ffc5abb4f664cb980eb1c92244e17d3356bbf
islamclucas <46931234+islamclucas@users.noreply.github.com>
2022-04-04 13:13:23 +0100

    node tests and messages

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -24,14 +24,33 @@ var leader = false;
 var rMQrunning =false;
 var amqp =require('amqplib/callback_api'); //in test folder
 var alive = true;
+var currentTime = new Date().toLocaleString(); //getting time 
 
-
+var nodeID = Math.floor(Math.random() * (100 -1 + 1)+1);
+var nodeInfo = {nodeID: nodeID, lastMessageTime: currentTime};
+var listNodeInfo=[];
+listNodeInfo.push(nodeID);//add current 
 
 
 setInterval(function() {
 
-  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+//  console.log(`Intervals are used to fire a function for the lifetime of an application.`);
+amqp.connect('ampq://user:bitnami@cloud_week1_haproxy_1'), function(error0, connection){
+  //if error
+  if (error0){
+    throw error0;
+  }
+//create connection and send message to alive nodes
 
+connection.createChannel(function(error1, channel){
+  if(error1){
+    throw error1;
+  }
+  var exchange = 'Node is alive';
+  currentTime = new Date().toLocaleString(); //getting time 
+ var message = '{"nodeID": "${nodeID}"}'
+});
+}
 }, 3000);
 
 //tell express to use the body parser. Note - This function was built into express but then moved to a seperate package.
